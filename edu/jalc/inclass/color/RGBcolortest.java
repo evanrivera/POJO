ba86175521077e2e0d45e4d602a3b702293258b9
evanrivera <evanmr98@yahoo.com>
2017-02-10 08:21:22 -0600

    Update RGBcolortest.java

--- edu/jalc/inclass/color/RGBcolortest.java
+++ edu/jalc/inclass/color/RGBcolortest.java
@@ -1,31 +1,31 @@
 package edu.jalc.inclass.color;
 
-public class RGBColorTest {
+public class RGBcolortest {
 
 	public void testGetRed(){
-		System.out.println("Testing RGBColor:getRed");
-		RGBColor rgbColor = new RGBColor((byte)1,(byte)1,(byte)1);
-		assert(rgbColor.getRed() == (byte)1);
+		System.out.println("Testing RGBcolor:getRed");
+		RGBcolor rgbcolor = new RGBcolor((byte)1,(byte)1,(byte)1);
+		assert(rgbcolor.getRed() == (byte)1);
 	}
 
 	public void testGetGreen(){
-		System.out.println("Testing RGBColor:getGreen");
-		RGBColor rgbColor = new RGBColor((byte)1,(byte)1,(byte)1);
-		assert(rgbColor.getRed() == (byte)1);
+		System.out.println("Testing RGBcolor:getGreen");
+		RGBcolor rgbcolor = new RGBcolor((byte)1,(byte)1,(byte)1);
+		assert(rgbcolor.getRed() == (byte)1);
 	}
 
 	public void testGetBlue(){
-		System.out.println("Testing RGBColor:getBlue");
-		RGBColor rgbColor = new RGBColor((byte)1,(byte)1,(byte)1);
-		assert(rgbColor.getRed() == (byte)1);
+		System.out.println("Testing RGBcolor:getBlue");
+		RGBcolor rgbcolor = new RGBcolor((byte)1,(byte)1,(byte)1);
+		assert(rgbcolor.getRed() == (byte)1);
 	}
 
 	public static void main(String... args){
-		RGBColorTest rgbColorTest = new RGBColorTest();
-		rgbColorTest.testGetRed();
-		rgbColorTest.testGetGreen();
-		rgbColorTest.testGetBlue();
+		RGBcolortest rgbcolortest = new RGBcolortest();
+		rgbcolortest.testGetRed();
+		rgbcolortest.testGetGreen();
+		rgbcolortest.testGetBlue();
 
-		System.out.println("RGBColorTest Completed");
+		System.out.println("RGBcolortest Completed");
 	}
 }
