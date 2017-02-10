c537fd9a6321008be3063748773abaf6c2c2534d
evanrivera <evanmr98@yahoo.com>
2017-02-10 08:33:32 -0600

    Update SodaBottleLabel.java

--- edu/jalc/inclass/label/SodaBottleLabel.java
+++ edu/jalc/inclass/label/SodaBottleLabel.java
@@ -1,1 +1,36 @@
+package edu.jalc.inclass.label;
 
+
+import edu.jalc.inclass.color.RGBcolor;
+import edu.jalc.inclass.logo.DrPepperLogo;
+
+public class SodaBottleLabel {
+
+	private final double height;
+	private final RGBcolor rgbcolor;
+	private final DrPepperLogo drPepperLogo;
+
+	private SodaBottleLabel(){
+		this.height = 0;
+		this.rgbcolor = null;
+		this.drPepperLogo = null;
+	}
+
+	public SodaBottleLabel(double height, RGBcolor rgbcolor, DrPepperLogo drPepperLogo){
+		this.height = height;
+		this.rgbcolor = rgbcolor;
+		this.drPepperLogo = drPepperLogo;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public RGBcolor getRGBcolor() {
+		return rgbcolor;
+	}
+
+	public DrPepperLogo getDrPepperLogo() {
+		return drPepperLogo;
+	}
+}
